d10682830896e089c18532314518f32883c63d7e
Beverly Appleton <bfcain19@gmail.com>
2020-03-31 17:16:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 #First solve it using `.each`
 #Then utilize the same method using `.collect` to see the difference.
 
-def reverse_each_word(string)
+def reverse_each_word_with_each(string)
   original_array = string.split(" ")
   return_array = []
   original_array.each do |string|
